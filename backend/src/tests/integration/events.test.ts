a898bd781720dffcb9983fce5e2b9dbfdcbe0586
Oscar Stickland <76094129+oscarstickland@users.noreply.github.com>
2026-08-22 16:28:07 +1000

    Remove docker pull

--- backend/src/tests/integration/events.test.ts
+++ backend/src/tests/integration/events.test.ts
@@ -22,6 +22,8 @@ describe("Database Integration Test", () => {
     });
 
     it("prevents standard users from creating events", async () => {
+        expect(true).toBe(false);
+
         const app = await harness.setupApp();
         const club = await harness.setupClub("Club");
         const { cookie } = await harness.setupUser("test@test.com", "standard", club.id, "User");
